bee99be95d2e34390264d9c9c0c2be0b29b846f7
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-06-08 11:24:33 -0400

    Initial commit

--- histogrammar/plot/bokeh.py
+++ histogrammar/plot/bokeh.py
@@ -16,13 +16,53 @@
 
 # "Public" methods; what we want to attach to the Histogram as a mix-in.
 
+from bokeh.charts.builder import Builder, create_and_build
+from bokeh.models import FactorRange, Range1d
+from bokeh.charts.glyphs import BarGlyph
+from bokeh.charts.properties import Dimension
+from bokeh.charts.attributes import ColorAttr, CatAttr
+from bokeh.models.sources import ColumnDataSource
+from bokeh.charts.builders.bar_builder import BarBuilder
+
 class HistogramMethods(object):
-    def bokeh(self):
-        import bokeh
+    def bokeh(self, label=None, values=None, color=None, stack=None, group=None, agg="sum",
+        xscale="categorical", yscale="linear", xgrid=False, ygrid=True,
+        continuous_range=None, **kw):
 
-        # ...
+        if continuous_range and not isinstance(continuous_range, Range1d):
+            raise ValueError(
+                    "continuous_range must be an instance of bokeh.models.ranges.Range1d"
+            )
 
-        return something
+        if label is not None and values is None:
+            kw['label_only'] = True
+            if (agg == 'sum') or (agg == 'mean'):
+                agg = 'count'
+                values = label
+
+        # The continuous_range is the y_range
+        y_range = continuous_range
+        kw['label'] = label
+        kw['values'] = values
+        kw['color'] = color
+        kw['stack'] = stack
+        kw['group'] = group
+        kw['agg'] = agg
+        kw['xscale'] = xscale
+        kw['yscale'] = yscale
+        kw['xgrid'] = xgrid
+        kw['ygrid'] = ygrid
+        kw['y_range'] = y_range
+
+        return create_and_build(BarBuilder, self.numericalValues, **kw)
+
+
+    def plot(self):
+        pass
+    def save(self):
+        pass
+    def view(self): 
+        pass
 
 class ProfileMethods(object):
     pass
