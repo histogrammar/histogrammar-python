a80f3461135930694c5189fc326b49689ac9b07b
Jim Pivarski <jpivarski@gmail.com>
2016-06-23 19:51:10 -0500

    start adding documentation

---
+++ docs/makeRST.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import importlib
+import inspect
+
+modules = [
+    "histogrammar.defs",
+    "histogrammar.specialized",
+    "histogrammar.util",
+    "histogrammar.version",
+    "histogrammar.primitives.absoluteerr",
+    "histogrammar.primitives.adaptivebin",
+    "histogrammar.primitives.average",
+    "histogrammar.primitives.bag",
+    "histogrammar.primitives.bin",
+    "histogrammar.primitives.categorize",
+    "histogrammar.primitives.centralbin",
+    "histogrammar.primitives.collection",
+    "histogrammar.primitives.count",
+    "histogrammar.primitives.deviate",
+    "histogrammar.primitives.fraction",
+    "histogrammar.primitives.limit",
+    "histogrammar.primitives.minmax",
+    "histogrammar.primitives.partition",
+    "histogrammar.primitives.quantile",
+    "histogrammar.primitives.sample",
+    "histogrammar.primitives.select",
+    "histogrammar.primitives.sparsebin",
+    "histogrammar.primitives.stack",
+    "histogrammar.primitives.sum",
+    "histogrammar.plot.bokeh",
+    "histogrammar.plot.root",
+    ]
+
+modules = {name: importlib.import_module(name) for name in modules}
+
+documented = []
+for moduleName, module in modules.items():
+    for objName in dir(module):
+        obj = getattr(module, objName)
+        if not objName.startswith("_") and callable(obj) and obj.__module__ == moduleName:
+            print objName, obj
+            documented.append(moduleName + "." + objName)
+            if inspect.isclass(obj):
+                open(moduleName + "." + objName + ".rst", "w").write('''
+{0}
+{1}
+
+.. autoclass:: {0}
+    :members:
+    :undoc-members:
+    :show-inheritance:
+'''.format(moduleName + "." + objName, "=" * (len(moduleName) + len(objName) + 1)))
+            else:
+                open(moduleName + "." + objName + ".rst", "w").write('''
+{0}
+{1}
+
+.. autofunction:: {0}
+'''.format(moduleName + "." + objName, "=" * (len(moduleName) + len(objName) + 1)))
+
+documented.sort()
+
+open("index.rst", "w").write('''
+Histogrammar |version|
+======================
+
+:ref:`genindex`
+
+.. toctree::
+   :maxdepth: 2
+
+''' + "\n".join("   " + x for x in documented) + "\n")
