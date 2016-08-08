e09c4f5fc1e1dc873ec5b0383ae77014fc1b76a1
Jim Pivarski <jpivarski@gmail.com>
2016-08-08 20:28:23 +0000

    first example of working reducer over thread-local variables

--- testcuda.cu
+++ testcuda.cu
@@ -1,76 +1,56 @@
 #include <stdio.h>
 
-namespace histogrammar {
-  typedef struct {
-    float values[10];
-  } Bn10CtCtCtCt;
+typedef struct {
+  float values[10];
+} Bn10CtCtCtCt;
 
-  typedef Aggregator Bn10CtCtCtCt;
+typedef Bn10CtCtCtCt Aggregator;
 
-  void __global__ initialize(Aggregator* aggregator) {
-    Aggregator *storage = &aggregator[threadIdx.x];
-    for (int i = 0;  i < 10;  ++i)
-      storage->values[threadIdx.x].values[i] = 12;
-  }
+extern __shared__ Aggregator threadLocal[];
 
-  void __global__ fill(Aggregator* aggregator, float* data, float* weights) {
-    float datum = data[blockIdx.x * blockDim.x + threadIdx.x];
-    float weight = weights[blockIdx.x * blockDim.x + threadIdx.x];
+void __global__ initialize() {
+  Aggregator* mine = &threadLocal[threadIdx.x + blockIdx.x*blockDim.x];
 
-
-
-
-  }
-
-  Aggregator *allocate(int numThreads) {
-    Aggregator *out;
-    cudaMalloc((void**)&out, numThreads * sizeof(Aggregator));
-    initialize<<<1, numThreads>>>(out);
-    return out;
-  }
-
-
-
-
+  for (int i = 0;  i < 10;  ++i)
+    mine->values[i] = 0;
 }
 
+void __global__ increment() {
+  Aggregator* mine = &threadLocal[threadIdx.x + blockIdx.x*blockDim.x];
+  for (int i = 0;  i < 10;  ++i)
+    mine->values[i] += 1;
+}
 
-namespace histogrammar {
-  class Aggregator {
-  public:
-    
+void __global__ extract(Aggregator* globalOnGPU) {
+  for (int i = 0;  i < 10;  ++i)
+    globalOnGPU->values[i] = 0;
 
+  for (int index = 0;  index < 100;  ++index) {
+    Aggregator* mine = &threadLocal[index];
 
-
-  };
-
-
-  typedef struct {
-    float test;
-  } Histogram;
-
-  __global__ void init(Histogram* histogram) {
-    const int index = threadIdx.x;
-    histogram[index].test = 5;
-  }
-
-  void fillall() {
-    Histogram* working;
-    Histogram output[100];
-
-    cudaMalloc((void**)&working, 100 * sizeof(Histogram));
-
-    histogrammar::init<<<1, 100>>>(working);
-
-    cudaMemcpy(output, working, 100 * sizeof(Histogram), cudaMemcpyDeviceToHost);
-
-    cudaFree(working);
-
-    for (int i = 0;  i < 100;  i++)
-      printf("%g\n", output[i].test);
+    for (int i = 0;  i < 10;  ++i)
+      globalOnGPU->values[i] += mine->values[i];
   }
 }
 
 int main(int argc, char** argv) {
-  histogrammar::fillall();
+    Aggregator *globalOnGPU = NULL;
+    Aggregator globalOnCPU;
+  
+    initialize<<<1, 100, 100 * sizeof(Aggregator)>>>();
+
+    increment<<<1, 100, 100 * sizeof(Aggregator)>>>();
+    increment<<<1, 100, 100 * sizeof(Aggregator)>>>();
+    increment<<<1, 100, 100 * sizeof(Aggregator)>>>();
+
+    cudaMalloc((void**)&globalOnGPU, sizeof(Aggregator));
+
+    extract<<<1, 1, 100 * sizeof(Aggregator)>>>(globalOnGPU);
+
+    cudaMemcpy(&globalOnCPU, globalOnGPU, sizeof(Aggregator), cudaMemcpyDeviceToHost);
+
+    cudaFree(globalOnGPU);
+
+    for (int i = 0;  i < 10;  i++)
+      printf("values[%d] = %g\n", i, globalOnCPU.values[i]);
 }
