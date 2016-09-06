3933e336266a909e39cea274d70f648cbd0eaeeb
Jim Pivarski <jpivarski@gmail.com>
2016-09-06 09:54:09 -0500

    GitHub's 1.0.2 is exactly the same as PyPI's 1.0.2 (clean-up cycle has converged).

--- histogrammar/version.py
+++ histogrammar/version.py
@@ -16,7 +16,7 @@
 
 import re
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 version = __version__
 
--- setup.py
+++ setup.py
@@ -19,14 +19,28 @@ from setuptools import setup, find_packages
 
 import histogrammar.version
 
-setup(name = "Histogrammar",
+setup(name = "histogrammar",
       version = histogrammar.version.__version__,
       packages = find_packages(),
       scripts = ["scripts/hgwatch"],
       description = "Composable histogram primitives for distributed data reduction.",
+      long_description = """Histogrammar is a suite of data aggregation primitives designed for use in parallel processing. In the simplest case, you can use this to compute histograms, but the generality of the primitives allows much more.
+
+See http://histogrammar.org for a complete introduction.
+
+This Python implementation of Histogrammar adheres to version 1.0 of the specification and has been tested to guarantee compatibility with the Scala implementation. The test suite includes empty datasets, NaN/infinity handling, associativity tests, and numerical agreement at the level of one part in a trillion (double precision). Several common histogram types can be plotted in Matplotlib, PyROOT, and Bokeh with a single method call.
+
+If Numpy or Pandas is available, histograms and other aggregators can be filled from arrays ten to a hundred times more quickly via Numpy commands, rather than Python for loops.
+
+If PyROOT is available, histograms and other aggregators can be filled from ROOT TTrees hundreds of times more quickly by JIT-compiling a specialized C++ filler.
+
+Histograms and other aggregators may also be converted into CUDA code for inclusion in a GPU workflow. And if PyCUDA is available, they can also be filled from Numpy arrays by JIT-compiling the CUDA.""",
       author = "Jim Pivarski (DIANA-HEP)",
       author_email = "pivarski@fnal.gov",
+      maintainer = "Jim Pivarski (DIANA-HEP)",
+      maintainer_email = "pivarski@fnal.gov",
       url = "http://histogrammar.org",
+      download_url = "https://github.com/histogrammar/histogrammar-python",
       license = "Apache Software License v2",
       test_suite = "tests",
       install_requires = [],
@@ -39,4 +53,5 @@ setup(name = "Histogrammar",
                      "Topic :: Scientific/Engineering :: Mathematics",
                      "Topic :: Scientific/Engineering :: Physics",
                      ],
+      platforms = "Any",
       )
