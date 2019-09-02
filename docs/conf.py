8ea131b56397b687bdc2fd856bb22641a25817a3
KX79WQ <max.baak@ing.com>
2019-09-02 13:00:02 +0200

    Bump up histrogrammar version to 1.0.10

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ copyright = u'2016, DIANA-HEP'
 # built documents.
 #
 # The short X.Y version.
-version = "1.0.9"
+version = "1.0.10"
 # The full version, including alpha/beta/rc tags.
 release = "1.0.9"
 
